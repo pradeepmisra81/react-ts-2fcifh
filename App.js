15f2227c7ec0b8bfa96dc15dd090a665991c88f8
pradeepmisra81 <pradeep.misra@gmail.com>
2022-05-17 11:03:22 +0530

    1 file modified

--- App.js
+++ App.js
@@ -4,20 +4,22 @@ import './style.css';
 import Select from 'react-select';
 
 export default function App() {
-  const options = [
-    { value: 'option1', label: 'Option1' },
-    { value: 'option2', label: 'Option2' },
-    { value: 'option3', label: 'Option3' },
-  ];
+  const [selectedOption, setSelectedOption] = useState('2019');
 
-  const [selectedOption, setSelectedOption] = useState(null);
+  const dropdownChangeHandler = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
   return (
     <div className="App">
-      <Select
-        defaultValue={selectedOption}
-        onChange={setSelectedOption}
-        options={options}
-      />
+      <label>Filter by year</label>
+      <select onClick={dropdownChangeHandler}>
+        <option value="2022">2022</option>
+        <option value="2021">2021</option>
+        <option value="2020">2020</option>
+        <option value="2019">2019</option>
+        <option value="2018">2018</option>
+      </select>
     </div>
   );
 }
